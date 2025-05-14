a3746a2d835a2bd116b66ed20040ee25db2786ef
Sikandar Afridi <sikanderafridi.854@gmail.com>
2025-05-14 23:24:18 +0500

    Add files via upload

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div className="container">
+      <h1>Cloud Computing Lab 12</h1>
+      <h2>Sikandar Afridi</h2>
+Z
+      <nav className="nav">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+
+      <h2>Navigation Header</h2>
+      <nav className="nav">
+        <a href="#">Home</a>    
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+export default App;
